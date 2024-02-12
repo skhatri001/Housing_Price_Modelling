aab653c0a3b62a9f461e84a91556b0381ef471ac
skhatri001 <121352864+skhatri001@users.noreply.github.com>
2024-02-11 19:22:59 -0500

    initial commit

---
+++ Mortgage.py
@@ -0,0 +1,9 @@
+class Mortgage:
+
+    def __init__(self,home_cost,loan_amount,interest_rate):
+        self.home_cost = home_cost
+        self.loan_amount = loan_amount
+        self.interest_rate = interest_rate
+        self.principle= home_cost - loan_amount
+
+    def 
