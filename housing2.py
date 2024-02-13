2e77ed1677b23f68c7632df4faa0bb31529482b1
Sanjay Khatri <skhatri@spacerake.net>
2024-02-12 20:27:24 -0500

    Update plot titles and moved input parameter locations to the top of the script

--- housing2.py
+++ housing2.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 from Mortgage import mortgage
 
 homeValue = 1e6 # Home price in dollars
-downPaymentFraction = 0.20 # 
+downPaymentFraction = 0.25 # 
 interestRate = 0.06
 n_years = 30
+monthly_extra_factor = 0.10
+annual_increase = 1.03
 t = np.arange(0,n_years,1) # Time years
 
 house_1 = mortgage(homeValue,downPaymentFraction,interestRate)
 downPayment = house_1.calcDownPayment()#homeValue*downPaymentFraction
 loanAmount = house_1.calcLoanAmount()#homeValue - downPayment
+
 t_months = np.arange(0,n_years*12)#t*12
 interestRate_Month = interestRate/12
 loanAmount_Growth=loanAmount*np.ones(len(t))
@@ -29,16 +32,17 @@ total_payment = np.zeros((len(t_months)))
 #     loanAmount_Growth[i] += interest[i-1]
 
 ## Monthly Interest Rate and Loan Amount Growth
-extra_factor = 0.10
-payment = (interestRate_Month)*loanAmount*(1+extra_factor)
+
+payment = (interestRate_Month)*loanAmount*(1+monthly_extra_factor)
 
 #print(payment)
 principle = loanAmount
+
 for i in range(1,len(t_months)):
     interest_Month[i] = (interestRate_Month)*principle
     monthly_remainder = payment-interest_Month[i]
     if i//12 == 1:
-        payment *= 1.03
+        payment *= annual_increase
         print(payment)
     #print(monthly_remainder)
     if monthly_remainder>0:
@@ -56,18 +60,19 @@ for i in range(1,len(t_months)):
 # plt.plot(t,(loanAmount_Growth-loanAmount_Growth[0])/1e6,marker='o',color='r',markersize=10,  label='Interest Growth')
 
 #plt.plot(t_months/12,interest_Month,marker='o',color='b',markersize=10, label='Total Amount Remaining')
-plt.plot(t_months[1:]/12,np.cumsum(total_payment)[1:]/1e6,marker='o',color='r',markersize=10,  label='Interest, Final Monthly Payment is $'+str(round(payment,1)) + ', '+str(int(extra_factor*100))+'%')
+plt.plot(t_months[1:]/12,np.cumsum(total_payment)[1:]/1e6,marker='o',color='r',markersize=10,  label='Final Monthly Payment is $'+str(round(payment,1)) + ', '+str(int(monthly_extra_factor*100))+'%')
 
 
 plt.xlabel('Time (Years)',fontsize=40)
-plt.ylabel('Total Amount Paid Toward Mortgage ($M)',fontsize=40)
+plt.ylabel('Mortgage Payment ($M)',fontsize=40)
 plt.xticks(fontsize=30)
 plt.yticks(fontsize=30)
 plt.axhline(y=0, c='k')
 # plt.axhline(y=2*homeValue/1e6, c='k')
 # plt.axhline(y=3*homeValue/1e6, c='k')
 plt.legend(loc='best',fontsize=34)
-plt.title(str(int(loanAmount_Growth[0]))+' Loan with ' + str(100*interestRate)+'%' + ' interest rate', fontsize=36)
+plt.title('Home value:$'+str(int(homeValue))+', Loan Value:\$'+str(int(loanAmount_Growth[0]))+', Interest Rate: ' + str(100*interestRate)+'%' + ', Annual Payment Increase: '+str(int((annual_increase-1)*100))+'%', fontsize=36)
+plt.grid(True)
 plt.show()
 
 ## Do something 
